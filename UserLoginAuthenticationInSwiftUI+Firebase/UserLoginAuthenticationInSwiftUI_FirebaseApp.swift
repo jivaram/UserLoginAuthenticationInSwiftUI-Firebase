64d998fcb67dd47661bfe1fa6cb7177cd186f5f3
jivaram <jivaramnbh@gmail.com>
2024-09-25 12:00:34 +0530

    Initial Commit

---
+++ UserLoginAuthenticationInSwiftUI+Firebase/UserLoginAuthenticationInSwiftUI_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserLoginAuthenticationInSwiftUI_FirebaseApp.swift
+//  UserLoginAuthenticationInSwiftUI+Firebase
+//
+//  Created by Jiva Ram on 25/09/24.
+//
+
+import SwiftUI
+
+@main
+struct UserLoginAuthenticationInSwiftUI_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
